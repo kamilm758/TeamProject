5122333f977c1551a77a9c71118e0b5b9752ec3b
Daniel Kamiński <daniel.kaminski1998@gmail.com>
2020-01-10 10:33:40 +0100

    commit

---
+++ TeamProject/TeamProject/Models/FormGeneratorModels/FieldToForms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamProject.Models.FormGeneratorModels
+{
+    public class FieldToForms
+    {
+        public int IdForms { get; set; }
+        public int IdField { get; set; }
+        public string expectedAnswer { get; set; }
+    }
+}
---
+++ TeamProject/TeamProject/Models/FormGeneratorModels/PatientForms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamProject.Models.FormGeneratorModels
+{
+    public class PatientForms
+    {
+        public int IdPatient { get; set; }
+        public int IdForm { get; set; }
+        public Boolean? agreement { get; set; }
+    }
+}
