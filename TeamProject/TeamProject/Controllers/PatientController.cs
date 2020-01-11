f6c81b19136a29f03bf9c2ece88b301c19f9d692
Sypa <Saker1919@gmail.com>
2020-01-11 21:30:16 +0100

    x

---
+++ TeamProject/TeamProject/Controllers/PatientController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FormGenerator.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TeamProject.Models.FormGeneratorModels;
+
+namespace TeamProject.Controllers
+{
+    public class PatientController : Controller
+    {
+
+        private readonly FormGeneratorContext _context;
+
+        public PatientController(FormGeneratorContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult AddPatient()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Patients.Add(patient);
+
+                var formsId =  _context.Forms;
+
+            }
+            return View();
+        }
+    }
+}
