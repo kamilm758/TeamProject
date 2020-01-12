cb359aeb6f081d3d96b27ec806f62c601b6e01b8
Sypa <Saker1919@gmail.com>
2020-01-12 22:46:15 +0100

    n
    nowy widok

--- TeamProject/TeamProject/Controllers/PatientController.cs
+++ TeamProject/TeamProject/Controllers/PatientController.cs
@@ -19,21 +19,38 @@ namespace TeamProject.Controllers
             _context = context;
         }
 
+        public async Task<IActionResult> PatientForms()
+        {
+            return View(await _context.PatientForms.ToListAsync());
+        }
+
+
+
         [HttpGet]
-        public IActionResult AddPatient()
+        public IActionResult Create()
         {
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id")] Patient patient)
+        public async Task<IActionResult> Create([Bind("IdPatient")] Patient patient)
         {
+            int y = patient.IdPatient; 
             if (ModelState.IsValid)
             {
                 _context.Patients.Add(patient);
 
                 var formsId =  _context.Forms;
+
+                foreach(var x in formsId)
+                {
+                    PatientForms patientForms = new PatientForms();
+                    patientForms.IdPatient = patient.IdPatient;
+                    patientForms.IdForm = x.Id;
+                    _context.PatientForms.Add(patientForms);
+                }
+                _context.SaveChanges();
 
             }
             return View();
